c65700f96834bd4073b4fa0b94ba9afbf86e2507
unknown <suto.bence_2022c@bolyai-szakkozep.hu>
2025-11-08 04:04:56 +0100

    konzolra kiirjuk a stringeket

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log(document.querySelector("title").innerText); // kiiratom konzolra a weboldal nevet
+console.log(document.querySelector("thead").innerHTML); // kiiratom konzolra a tablazat fejlecet
+console.log(document.querySelector("tbody tr:first-child").innerHTML); // kiiratom konzolra a tablazat elso sorat 
+console.log(document.querySelector("tr:nth-child(2)").innerHTML); // kiiratom konzolra a tablazat masodik sorat 
+console.log(document.querySelector("tr:nth-child(3)").innerHTML); // kiiratom konzolra a harmadik sorat 
