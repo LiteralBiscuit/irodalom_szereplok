3dd7f030aa17d44d71dea8686ded5e7a07ada65d
unknown <suto.bence_2022c@bolyai-szakkozep.hu>
2025-11-08 22:25:10 +0100

    fuggveny bevezetese

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ table.append(thead); // fej hozzáfűzése a táblázathoz
 const tbody = document.createElement("tbody"); // a táblázat törzsének létrehozása és tárolása egy vátozóban
 table.appendChild(tbody); // törzs hozzáfűzése a táblázathoz
 
-// fej sor létrehozása
 /**
  * @type {HTMLTableRowElement} a fej sor
  */
@@ -62,16 +61,12 @@ const tableHeaderRow = document.createElement("tr"); // fej sor létrehozása
  * @type {number} index változó
  */
 let index = 0; // index változó létrehozása hogy a harmadik elemet meg lehessen határozni
-for (const header of tableheaderArray) { // for loop fej sor tartalmának a létrehozásához
+for (const header of tableheaderArray) { // for loop a fej sor tartalmának a létrehozásához
     index++; // index növelése
-    /**
-     * @type {HTMLTableCellElement} th
-     */
-    const tableHeaderCell = document.createElement("th"); // adott th létrehozása
-    tableHeaderCell.innerText = header.title; // adott th szövegének megadása
-    tableHeaderRow.appendChild(tableHeaderCell); // th hozzáfűzése a fej sorhoz
-    if(index == 3) // csak így jutott eszembe megoldani
-        tableHeaderCell.colSpan = 2; // 3. th colspan tulajdonságának beállítása 2-re
+    if(index == 3) // ha a 3. cella (csak így jutott eszembe megoldani)
+        createCell("th", header.title, tableHeaderRow, 2); // 3. cella létrehozása colSpan = 2 -vel
+    else // ha nem a 3. cella
+        createCell("th", header.title, tableHeaderRow); // adott cella létrehozása
 }
 thead.appendChild(tableHeaderRow); // fej sor hozzáfűzése a fejhez
 
@@ -80,34 +75,36 @@ for (const sor of tableBodyArray) { // for loop a tartalom léterehozásához
     /**
      * @type {HTMLTableRowElement} sor
      */
-    const tbodyRow = document.createElement("tr"); // adott sor létrehozása
-    tbody.appendChild(tbodyRow); // adott sor hozzáfűzése a tbody-hoz
-    /**
-     * @type {HTMLTableCellElement} 1. cella
-     */
-    const elsoTbodyCell = document.createElement("td"); // 1. cella létrehozása
-    tbodyRow.appendChild(elsoTbodyCell); // 1. clella hozzáfűzése az adott sorhoz
-    elsoTbodyCell.innerText = sor.szerzo; // 1. cella szövegének megadása
-    /**
-     * @type {HTMLTableCellElement} 2. cella
-     */
-    const masodikTbodyCell = document.createElement("td"); // 2. cella létrehozása
-    tbodyRow.appendChild(masodikTbodyCell); // 2. clella hozzáfűzése az adott sorhoz
-    masodikTbodyCell.innerText = sor.mu; // 2. cella szövegének megadása
-    /**
-     * @type {HTMLTableCellElement} 3. cella
-     */
-    const harmadikTbodyCell = document.createElement("td"); // 3. cella létrehozása
-    tbodyRow.appendChild(harmadikTbodyCell); // 3. clella hozzáfűzése az adott sorhoz
-    harmadikTbodyCell.innerText = sor.elsoSzereplo; // 3. cella szövegének megadása
-    if (sor.masodikSzereplo){ // ha van 4. sor
-        /**
-         * @type {HTMLTableCellElement} 4. cella
-         */
-        const negyedikTbodyCell = document.createElement("td"); // 4. cella létrehozása
-        tbodyRow.appendChild(negyedikTbodyCell); // 4. clella hozzáfűzése az adott sorhoz
-        negyedikTbodyCell.innerText = sor.masodikSzereplo; // 4. cella szövegének megadása
-        continue; // ha van 4. cella akkor nem állítunk colspant hanem visszaugrunk a ciklus elejére
+    const tbodyRow = document.createElement("tr"); // sor létrehozása
+    tbody.appendChild(tbodyRow); // sor hozzáfűzése a táblázathoz
+    if (sor.masodikSzereplo){ // ha van 4. cella
+        createCell("td", sor.szerzo, tbodyRow); // 1. cella létrehozása
+        createCell("td", sor.mu, tbodyRow); // 2. cella létrehozása
+        createCell("td", sor.elsoSzereplo, tbodyRow); // 3. cella létrehozása
+        createCell("td", sor.masodikSzereplo, tbodyRow); // 4. cella létrehozása
     }
-    harmadikTbodyCell.colSpan = 2; // 3. cella colspan beállítása 2-re ha nincs 4. sor
+    else{ // ha nincs 4. cella
+        createCell("td", sor.szerzo, tbodyRow); // 1. cella létrehozása
+        createCell("td", sor.mu, tbodyRow); // 2. cella létrehozása
+        createCell("td", sor.elsoSzereplo, tbodyRow, 2); // 3. cella létrehozása colSpan = 2 -vel
+    }
 }
+
+/**
+ * 
+ * @param {string} cellType th vagy td 
+ * @param {string} cellContent a cella tartalma
+ * @param {HTMLTableRowElement} parentRow a sor ahova bemegy a csinált cella 
+ * @param {number} colspan a cella colspanja alapesetben 1
+ * @returns {void} nem tér vissza a megcsinált cellával
+ */
+function createCell (cellType, cellContent, parentRow, colspan = 1){ // cellát létrehozó függvény
+    /**
+     * @type {HTMLTableCellElement} a létrehozandó cella
+     */
+    const cell = document.createElement(cellType); // cella létrehozása
+    cell.innerText = cellContent; // cella tartalmának beállítása
+    cell.colSpan = colspan; // colspan beállítása
+    parentRow.appendChild(cell); // cella hozzáadása a szülősorhoz
+}
+// őszinte vélemény: ilyet kérem soha többet tanár úr! minden sorba komment az nagyon extrém és erősen az öngyilkosságra ösztönöz
